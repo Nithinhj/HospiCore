e7100ef47d8e2cbe1bfff699a1850207e392daea
nithi <nithi@ASUS>
2026-05-01 11:54:09 +0530

    Updated Admin With Manage Room5/1/26

--- HospiCore/src/Hospicore/Login.java
+++ HospiCore/src/Hospicore/Login.java
@@ -39,7 +39,36 @@ int choice;
 					 choice=scanner.nextInt();
 					switch (choice) {
 					case 1:
+						System.out.println("Enter Room Number");
+						int roomNumb=scanner.nextInt();
+						scanner.nextLine();
+						System.out.println("Enter Room Type");
+						String roomType= scanner.nextLine();
+						System.out.println("Enter Room Price");
+						double roomPrice=scanner.nextDouble();
+						String insertQuery="insert into room (Room_Number,Room_Type,Price,is_available,is_clean) values(?,?,?,?,?);";
+						PreparedStatement ps=con.prepareStatement(insertQuery);
+						ps.setInt(1, roomNumb);
+						ps.setString(2, roomType);
+ps.setDouble(3, roomPrice);
+ps.setBoolean(4, true);
+ps.setBoolean(5, true);
+int rows=ps.executeUpdate();
+if (rows>0)//to check if room updated or not {
+	System.out.println("Room Updated SuccessFully");
+
+
+	
+else {
+	System.out.println("Room updation failed");
+
+}
+
 						
+						
+
+
+
 						
 						break;
 			case 2:
@@ -62,13 +91,15 @@ int choice;
 				} while (choice!=3);
 				
 				
-			}else {
-				System.out.println("Welcome to HospiCore");
+			}else if(role.equalsIgnoreCase("Front_Office")) {
+				System.out.println("Welcome to HospiCore Front Office");
 				int choice;
 				do {
 				
 					System.out.println("1.Booking");
-					System.out.println("2.Exit");
+					System.out.println("2.Check out");
+
+					System.out.println("3.Exit");
 					System.out.println("Enter your Choice");
 					 choice=scanner.nextInt();
 					switch (choice) {
@@ -77,7 +108,9 @@ int choice;
 						
 						
 						break;
-			case 2:
+						case 2:
+							break;
+			case 3:
 				System.out.println("Exiting...");
 
 				
@@ -91,16 +124,127 @@ int choice;
 
 
 					
-				} while (choice!=2);
+				} while (choice!=3);
 				}
 				
 			
 			
 				
+else if (role.equalsIgnoreCase("House_Keepng")) {
+	System.out.println("Welcome to HospiCore House Keeping");
+	int choice;
+	do {
+		System.out.println("1.View All Room ");
+		System.out.println("2. Mark Clean Rooms");
+		System.out.println("3. Mark Dirty Rooms");
+		System.out.println("4.Exiting..");
+		System.out.println("Enter your choice");
+		choice=scanner.nextInt();
+		
+		switch (choice) {
+		case 1:
+			
+			break;
+			case 2: 
+				break;
+				case 3:
+					break;
+					case 4:
+						System.out.println("Exiting...");
+						break;
+
+
+		default:
+			break;
+		}
+
+
+
 
 		
+	} while (choice !=4);
+
 	
-	}else {
+}
+			else if (role.equalsIgnoreCase("Restaurent")) {
+				System.out.println("Welcome to HospiCore Restaurent ");
+				int choice;
+
+				do {
+					System.out.println("1.K.O.T");
+					System.out.println("2.Billing");
+					System.out.println("3.Settlement");
+					System.out.println("4.Exit");
+
+					System.out.println("Enter Your Choice");
+					choice =scanner.nextInt();
+
+
+
+					switch (choice) {
+					case 1: 
+						
+						break;
+						case 2:
+							break ;
+						case 3:
+					break;
+					case 4 :
+						System.out.println("Exiting...");
+
+					default:
+						break;
+					}
+
+					
+					
+				} while (choice!=4);
+				
+			}
+		else if (role.equalsIgnoreCase("Store")) {
+			System.out.println("Welcome to HospiCore Store");
+			
+			int choice;
+			do {
+				System.out.println();
+				System.out.println("1.Add Items");
+				System.out.println("2.View Items");
+				System.out.println("3.View Requests");
+				System.out.println("4.Issue Items");
+				System.out.println("5.Exit");
+				System.out.println("Enter your choice");
+				choice=scanner.nextInt();
+				switch (choice) {
+				case 1:
+					
+					break;
+					case 2:
+					break;
+					case 3:
+						
+						break;
+						case 4:
+						break;
+						case 5:System.out.println("Exiting....");
+
+							break;
+					
+					default:
+					break;
+				}
+
+				
+				
+			} while (choice!=5);
+
+			
+		}
+	
+	}
+	
+	
+	
+	else {
 		System.out.println("Invalid Login");
 
 	}
